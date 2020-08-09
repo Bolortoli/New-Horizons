9a1c64e8862e0726b6864af5f9d100f14f8b5342
Bolortoli <boogiibobko@gmail.com>
2020-08-09 20:09:30 +0800

    Initialized iframe option

--- new_horizons/horizonapp/urls.py
+++ new_horizons/horizonapp/urls.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('panaroma/', TemplateView.as_view(template_name='panaroma.html'), name='panaroma')
+    path('panaroma/', views.panaroma, name='panaroma')
 ]
--- new_horizons/horizonapp/views.py
+++ new_horizons/horizonapp/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.views.decorators.clickjacking import xframe_options_exempt
-
+from django.views.decorators.clickjacking import xframe_options_sameorigin
 # Create your views here.
 @xframe_options_exempt
 def index(request):
     return render(request, 'home.html')
 
-# @xframe_options_exempt
-# def panaroma(request):
-#     return render(request, 'panaroma.html')
+@xframe_options_sameorigin
+def panaroma(request):
+    return render(request, 'panaroma.html')
